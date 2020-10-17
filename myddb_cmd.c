db10b9852b8f4a8763397bfde254b2d8d4554b57
Alberto F Jr <35114960+skapaflow@users.noreply.github.com>
2020-10-17 01:59:09 -0300

    Update myddb_cmd.c

--- myddb_cmd.c
+++ myddb_cmd.c
@@ -1,1 +1,46 @@
+#include "src/myddb.h"
 
+int main (int argc, char const *argv[]) {
+
+	char *str = malloc(sizeof(char) * DDBMAX);
+
+	myddb_init();
+
+	if (argc > 1) {
+		*str = '\0';
+		for (int i = 1; i < argc; i++) {
+			strcat(str, argv[i]);
+			strcat(str, " ");
+		}
+		myddb(str);
+		goto main_exit;
+	}
+
+	for (int i = 0; myddb_logo[i]; i++)
+		printf(" %s\n", myddb_logo[i]);
+	putchar(10);
+
+	while (true) {
+
+		printf("MyDDB: ");
+
+		fgets(str, DDBMAX, stdin);
+		str[strlen(str)-1] = '\0';
+
+		if (!strcmp(str, "close") || *str == '.')
+			break;
+
+		myddb(str);
+
+		for (int i = 0; omyddb && (i < omyddb->olot); i++)
+			printf(" >>> %s\n", omyddb[i].out);
+		putchar(10);
+	}
+
+main_exit:
+
+	free(str);
+	myddb_close();
+
+	return EXIT_SUCCESS;
+}
