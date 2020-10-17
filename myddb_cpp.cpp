e5038cbeb3951326a5e2d2bb0a55f5ffef6b5068
Alberto F Jr <35114960+skapaflow@users.noreply.github.com>
2020-10-17 12:06:35 -0300

    Update myddb_cpp.cpp

--- myddb_cpp.cpp
+++ myddb_cpp.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "myddb.h"
+#include "src/myddb.h"
 
 using namespace std;
 
 int main (void) {
 	/*
-		g++ -o myddb_cpp.exe myddb_cpp.cpp myddb.c -s -Wall
+		g++ -o myddb_cpp.exe myddb_cpp.cpp src/myddb.c -s -Wall
 	*/
 	cout << "\n MyDDB CPP!\n";
 
