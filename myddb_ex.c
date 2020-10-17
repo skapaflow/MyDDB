628e962a7c5782a2df4f71ec853a37a8497f79a8
Alberto F Jr <35114960+skapaflow@users.noreply.github.com>
2020-10-17 11:32:40 -0300

    Add files via upload

---
+++ myddb_ex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "myddb.h"
+
+int main (void) {
+	/*
+		gcc -o myddb_ex.exe myddb_ex.c myddb.c -s -Wall | myddb_ex.exe
+	*/
+	printf("\n MyDDB C\n");
+
+	myddb_init();
+	myddb("PATH ../data FROM helloworld PRINT 1");
+	myddb_close();
+
+	return 0;
+}
